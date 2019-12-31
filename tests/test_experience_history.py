ca436e65e4b51876ae98ea9e046e78b8c408a0b0
Joshua Lynch <jlynch@bnl.gov>
2019-12-31 15:59:35 -0500

    format with black

--- tests/test_experience_history.py
+++ tests/test_experience_history.py
@@ -13,14 +13,16 @@ def test_experience_history():
     all_rewards = np.array([0.0, 1.0, 0.0, 0.5, 1.0])
     all_done = np.array([False, False, False, True, False])
 
-    h = ExperienceHistory(state_shape=(all_states.shape[1], ), history_size=5, batch_size=3)
+    h = ExperienceHistory(
+        state_shape=(all_states.shape[1],), history_size=5, batch_size=3
+    )
 
     for i in range(5):
         state0 = all_states[i]
         action = all_actions[i]
         reward = all_rewards[i]
         done = all_done[i]
-        state1 = all_states[i+1]
+        state1 = all_states[i + 1]
         h.append(state0=state0, action=action, reward=reward, done=done, state1=state1)
 
         print(f"state0: {state0}")
@@ -32,15 +34,21 @@ def test_experience_history():
 
     assert len(h) == 5
 
-    state0_batch, action_batch, reward_batch, done_batch, state1_batch = h.sample_batch()
+    (
+        state0_batch,
+        action_batch,
+        reward_batch,
+        done_batch,
+        state1_batch,
+    ) = h.sample_batch()
 
     assert state0_batch.shape == (3, 2)
     assert state0_batch.dtype == np.float64
-    assert action_batch.shape == (3, )
+    assert action_batch.shape == (3,)
     assert action_batch.dtype == np.int
-    assert reward_batch.shape == (3, )
+    assert reward_batch.shape == (3,)
     assert reward_batch.dtype == np.float64
-    assert done_batch.shape == (3, )
+    assert done_batch.shape == (3,)
     assert done_batch.dtype == np.bool
     assert state1_batch.shape == (3, 2)
     assert state1_batch.dtype == np.float64
@@ -50,4 +58,3 @@ def test_experience_history():
     print(reward_batch)
     print(done_batch)
     print(state1_batch)
-
